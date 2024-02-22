c864db08e066191c9edca085001c1d0f6ce0c888
vinayak501 <vinayakchavda2004@gmail.com>
2024-02-23 00:11:22 +0530

    daily +2

---
+++ Top_View_Of_BT.java
@@ -0,0 +1,13 @@
+package org.example;
+
+class Pair{
+    TreeNode node;
+    int vertical;
+    public Pair(TreeNode n, int v){
+        node=n;
+        vertical=v;
+    }
+}
+public class Top_View_Of_BT {
+
+}

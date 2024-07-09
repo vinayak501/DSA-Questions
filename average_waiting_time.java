042712d6d943945985e3eab893663b8239f868b4
vinayak501 <vinayakchavda2004@gmail.com>
2024-07-10 00:26:34 +0530

    daily

---
+++ average_waiting_time.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class average_waiting_time {
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length;
+
+        double totalWaitTime = 0;
+        int currTime = 0;
+
+        for (int[] customer : customers) {
+            int arrivalTime = customer[0];
+            int cookTime = customer[1];
+
+            if (currTime < arrivalTime) {
+                currTime = arrivalTime;
+            }
+
+            int waitTime = currTime + cookTime - arrivalTime;
+
+            totalWaitTime += waitTime;
+
+            currTime += cookTime;
+        }
+
+        return totalWaitTime / n;
+    }
+}
